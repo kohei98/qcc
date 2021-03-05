45d6ac963fa6c6f452213293bb6eb88ee88f39a1
kohei98 <kohei0502.com@gmail.com>
2021-03-06 04:29:11 +0900

    パーサーを実装

--- 9cc.c
+++ 9cc.c
@@ -97,7 +97,8 @@ Token *tokenize(char *p) {
             continue;
         }
 
-        if (*p == '+' || *p == '-') {
+        if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' ||
+            *p == ')') {
             cur = new_token(TK_RESERVED, cur, p++);
             continue;
         }
@@ -113,33 +114,125 @@ Token *tokenize(char *p) {
     return head.next;
 }
 
+typedef enum {
+    ND_ADD,  // +
+    ND_SUB,  // -
+    ND_MUL,  // *
+    ND_DIV,  // /
+    ND_NUM,  //　整数
+} NodeKind;
+
+typedef struct Node Node;  // structの省略
+
+struct Node {
+    NodeKind kind;  //ノードの型
+    Node *lhs;      //左辺
+    Node *rhs;      //右辺
+    int val;
+};
+
+Node *primary();
+Node *mul();
+Node *expr();
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+// primary = "(" expr ")" | num
+Node *primary() {
+    // '('が来たら"(" expr() ")"のハズ
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+        //ほかは数値のハズ
+    } else
+        return new_node_num(expect_number());
+}
+
+// mul = primary("*" primary | "/" primary) *
+Node *mul() {
+    Node *node = primary();
+
+    for (;;) {
+        if (consume('*'))
+            node = new_node(ND_MUL, node, primary());
+        else if (consume('/'))
+            node = new_node(ND_DIV, node, primary());
+        else
+            return node;
+    }
+}
+
+// expr = mul("+" mul | "-" mul) *
+Node *expr() {
+    Node *node = mul();
+
+    for (;;) {
+        if (consume('+'))
+            node = new_node(ND_ADD, node, mul());
+        else if (consume('-'))
+            node = new_node(ND_SUB, node, mul());
+        else
+            return node;
+    }
+}
+
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("    push %d\n", node->val);
+        return;
+    }
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("    pop rdi\n");  // lhsの値
+    printf("    pop rax\n");  // rhsの値
+    switch (node->kind) {
+        case ND_ADD:
+            printf("    add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("    sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf("    imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            printf("    cqo\n");
+            printf("    idiv rdi\n");
+            break;
+    }
+    printf("    push rax\n");
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "引数の個数が正しくありません\n");
         return 1;
     }
-    // トークナイズする
+    // トークナイズしてパースする．
     user_input = argv[1];
     token = tokenize(argv[1]);
-
+    Node *node = expr();
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
 
-    // 式の最初は数でなければならないので，それをチェックして
-    // 最初のmov命令を出力
-    printf("    mov rax, %d\n", expect_number());
+    gen(node);
 
-    //　'+<数>'または'-<数>'というトークンの並びを消費しつつ
-    // アセンブリを出力
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("    add rax, %d\n", expect_number());
-            continue;
-        }
-        expect('-');
-        printf("    sub rax, %d\n", expect_number());
-    }
+    printf("    pop rax\n");
     printf("    ret\n");
     return 0;
 }
--- test.sh
+++ test.sh
@@ -19,4 +19,10 @@ assert (){
 assert 0 0 
 assert 42 42
 assert 21 "5+20-4"
+assert 41 " 12 + 34 - 5"
+assert 105 " 3 * 5  + (10 + 20) * 3"
+assert 4 "2 + 12 / 6"
+assert 47 '5+6*7'
+assert 15 '5*(9-6)'
+assert 4 '(3+5)/2'
 echo OK

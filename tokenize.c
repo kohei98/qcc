eac9aa849e1d4489834350603dba84749ba96114
kohei98 <kohei0502.com@gmail.com>
2021-04-23 02:50:05 +0900

    久しぶりに確認とインデント

--- tokenize.c
+++ tokenize.c
@@ -1,21 +1,23 @@
 #include "qcc.h"
 
-char *user_input;  //入力の先頭
+char *user_input; //入力の先頭
 Token *token;
 
-static bool is_alpha(char c) {
+static bool is_alpha(char c)
+{
     return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_';
 }
 
 static bool is_alnum(char c) { return is_alpha(c) || ('0' <= c && c <= '9'); }
 
 // エラーを報告する関数の改良版
-void error_at(char *loc, char *fmt, ...) {
+void error_at(char *loc, char *fmt, ...)
+{
     va_list ap;
     va_start(ap, fmt);
 
     int pos = loc - user_input;
-    fprintf(stderr, "%s\n", user_input);  //入力を表示
+    fprintf(stderr, "%s\n", user_input); //入力を表示
     fprintf(stderr, "%*s", pos, " ");
     fprintf(stderr, "^ ");
     vfprintf(stderr, fmt, ap);
@@ -25,7 +27,8 @@ void error_at(char *loc, char *fmt, ...) {
 
 //エラーを報告する関数
 // printfと同じ引数を取る
-void error(char *fmt, ...) {
+void error(char *fmt, ...)
+{
     va_list ap;
     va_start(ap, fmt);
     vfprintf(stderr, fmt, ap);
@@ -34,11 +37,13 @@ void error(char *fmt, ...) {
 }
 //次のトークンが期待している記号のときはトークンを一つ読み進めて
 //真を返す
-bool consume(char *op) {
+bool consume(char *op)
+{
     if (token->kind != TK_RESERVED ||
         strlen(op) !=
-            token->len ||  //次のトークンが期待している記号の長さでない時
-        memcmp(token->str, op, token->len)) {  // 長さが同じでも違う記号の時
+            token->len || //次のトークンが期待している記号の長さでない時
+        memcmp(token->str, op, token->len))
+    { // 長さが同じでも違う記号の時
         return false;
     }
     token = token->next;
@@ -46,21 +51,26 @@ bool consume(char *op) {
 }
 // 次のトークンが期待している記号のときはトークンを一つ読み進めて
 //それ以外はエラーを返す
-void expect(char *op) {
+void expect(char *op)
+{
     if (token->kind != TK_RESERVED || strlen(op) != token->len ||
         memcmp(token->str, op, token->len))
         error_at(token->str, "'%s'ではありません", op);
     token = token->next;
 }
-Token *consume_ident() {
-    if (token->kind != TK_IDENT) return NULL;
+Token *consume_ident()
+{
+    if (token->kind != TK_IDENT)
+        return NULL;
     Token *t = token;
-    token = token->next;  //一つすすめる
+    token = token->next; //一つすすめる
     return t;
 }
 
-Token *consume_tktype(TokenKind TK_KIND) {
-    if (token->kind != TK_KIND) return NULL;
+Token *consume_tktype(TokenKind TK_KIND)
+{
+    if (token->kind != TK_KIND)
+        return NULL;
     Token *t = token;
     token = token->next;
     return t;
@@ -68,8 +78,10 @@ Token *consume_tktype(TokenKind TK_KIND) {
 
 //次のトークンが数値の場合，トークンを一つ読み進めてその数値を返す
 //それ以外はエラーを返す
-int expect_number() {
-    if (token->kind != TK_NUM) error_at(token->str, "数ではありません");
+int expect_number()
+{
+    if (token->kind != TK_NUM)
+        error_at(token->str, "数ではありません");
     int val = token->val;
     token = token->next;
     return val;
@@ -78,7 +90,8 @@ int expect_number() {
 bool at_eof() { return token->kind == TK_EOF; }
 
 //新しいトークンを作成してcurにつなげる
-Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
+Token *new_token(TokenKind kind, Token *cur, char *str, int len)
+{
     Token *tok = calloc(1, sizeof(Token));
     tok->kind = kind;
     tok->str = str;
@@ -87,53 +100,63 @@ Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
     return tok;
 }
 
-bool startswith(char *p, char *q) {
+bool startswith(char *p, char *q)
+{
     return memcmp(p, q, strlen(q)) == 0;
-}  // pの接頭辞とqが一致しているかを判定
+} // pの接頭辞とqが一致しているかを判定
 
-// 入力文字pをトークナイズしてそれを返す
-Token *tokenize(char *p) {
+// 入力文字列pをトークナイズしてそれを返す
+Token *tokenize(char *p)
+{
     Token head;
     head.next = NULL;
     Token *cur = &head;
 
-    while (*p) {
+    while (*p)
+    {
         // 空白文字をスキップ
-        if (isspace(*p)) {
+        if (isspace(*p))
+        {
             p++;
             continue;
         }
 
         if (startswith(p, "==") || startswith(p, "!=") || startswith(p, "<=") ||
-            startswith(p, ">=")) {
+            startswith(p, ">="))
+        {
             cur = new_token(TK_RESERVED, cur, p, 2);
             p += 2;
             continue;
         }
 
-        if (strchr(";+-*/()<>=", *p)) {
+        if (strchr(";+-*/()<>=", *p))
+        {
             cur = new_token(TK_RESERVED, cur, p++, 1);
             continue;
         }
 
         // returnかどうかを調べる
-        if (strncmp(p, "return", 6) == 0 && !is_alnum(p[6])) {
+        if (strncmp(p, "return", 6) == 0 && !is_alnum(p[6]))
+        {
             cur = new_token(TK_RETURN, cur, p += 6, 6);
             continue;
         }
 
         // 識別子
-        if (is_alpha(*p)) {
+        if (is_alpha(*p))
+        {
             char *q = p++;
-            while (is_alnum(*p)) p++;
+            while (is_alnum(*p))
+                p++;
             cur = new_token(TK_IDENT, cur, q, p - q);
             continue;
         }
 
-        if (isdigit(*p)) {  //整数のとき
+        if (isdigit(*p))
+        { //整数のとき
             cur = new_token(TK_NUM, cur, p, 0);
             char *q = p;
-            cur->val = strtol(p, &p, 10);
+            cur->val = strtol(p, &p, 10); //ポインタのポインタを渡して返り値にするアレ
             cur->len = p - q;
             // printf("%s\n", cur->str);
             continue;
